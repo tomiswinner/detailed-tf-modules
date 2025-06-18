b0804bd42ff1d07362bfcb780d21ff0968d5c596
Takuto Oshima <takuto.oshima@tohmatsu.co.jp>
2025-06-18 10:37:02 +0900

    ok

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -115,6 +115,29 @@ resource "aws_autoscaling_group" "example" {
 }
 
 
+# sked を使ったASGのスケーリング, prd だけ実装する
+resource "aws_autoscaling_schedule" "scale_out_during_business_hours" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "scale-out-during-business-hours"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 10
+  recurrence = "0 9 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+resource "aws_autoscaling_schedule" "scale_in_at_night" {
+  count = var.enable_autoscaling ? 1 : 0
+  scheduled_action_name = "scale-in-at-night"
+  min_size = 2
+  max_size = 10
+  desired_capacity = 2
+  recurrence = "0 17 * * *"
+  autoscaling_group_name = module.webserver_cluster.asg_name
+}
+
+
+
 
 # ALB Security Group
 resource "aws_security_group" "alb" {
